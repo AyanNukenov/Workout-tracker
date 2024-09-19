d8a7fa431210a91416d7cefa9ce506107a902d1a
Ayan Nukenov <aann04185.gmail.com>
2024-09-19 12:29:30 +0500

    Обновление компонента WorkoutItem

--- src/components/WorkoutItem.js
+++ src/components/WorkoutItem.js
@@ -4,7 +4,7 @@ const WorkoutItem = ({ workout, onRemove }) => {
   return (  
     <div className="workout-item">  
       <h3>{workout.exercise}</h3>  
-      <p>{`Повторения: ${workout.reps}, Сеты: ${workout.sets}, Вес: ${workout.weight} кг`}</p>  
+      <p>{`Повторения: ${workout.reps}, сеты: ${workout.sets}, вес: ${workout.weight} кг`}</p>  
       <p>{`Дата: ${workout.date}`}</p>
       <button onClick={onRemove} className="add-button">Удалить</button>
     </div>  
