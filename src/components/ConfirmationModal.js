268f51e3d0b524f3785db9321ac88974b862cff1
Ayan Nukenov <aann04185.gmail.com>
2024-09-20 18:50:33 +0500

    Обновлены стили в ConfirmationModal для увеличения расстояния между кнопками

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -7,7 +7,7 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">  
       <div className="bg-white p-4 rounded shadow-lg">  
         <h2 className="text-lg font-bold mb-4">Вы уверены, что хотите удалить тренировку?</h2>  
-        <div className="flex justify-end">  
+        <div className="flex justify-end space-x-5">  
           <button  
             className="bg-red-500 text-white px-4 py-2 rounded mr-2"  
             onClick={onConfirm}  
